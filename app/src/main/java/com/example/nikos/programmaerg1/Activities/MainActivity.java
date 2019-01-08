5b25df7c85781790f17a1fc0c64e86c48d281b2f
NeekawsSk <45759982+NeekawsSk@users.noreply.github.com>
2019-01-08 02:49:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nikos/programmaerg1/Activities/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.nikos.programmaerg1.Activities;
+
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.nikos.programmaerg1.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvDiscussionTopics;
+    ArrayList<String> listOfDiscussion = new ArrayList<String>();
+    ArrayAdapter arrayAdpt;
+
+    String UserName;
+
+    private DatabaseReference dbr = FirebaseDatabase.getInstance().getReference().getRoot();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        lvDiscussionTopics = (ListView) findViewById(R.id.discussionTopics);
+        arrayAdpt = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, listOfDiscussion);
+        lvDiscussionTopics.setAdapter(arrayAdpt);
+
+
+        getUserName();
+
+
+        dbr.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+
+                while(i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
+                }
+
+                arrayAdpt.clear();
+                arrayAdpt.addAll(set);
+                arrayAdpt.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        lvDiscussionTopics.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), ChatActivity.class);
+                i.putExtra("selected_topic", ((TextView)view).getText().toString());
+                i.putExtra("user_name", UserName);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void getUserName(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final EditText userName = new EditText(this);
+
+        builder.setView(userName);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                UserName = userName.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                getUserName();
+            }
+        });
+        builder.show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case  R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                return true;
+        }
+
+        return false;
+    }
+
+}
